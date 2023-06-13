96bde08dbd25d59966f14c5769efbf81b1146e69
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-06-13 10:47:40 -0700

    refactor code using with block for connection

--- main.py
+++ main.py
@@ -5,10 +5,9 @@ my_email = "giemmatthew@gmail.com"
 password = password
 send_email = email
 
-connection = smtplib.SMTP("smtp.gmail.com", 587)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email,
-                    to_addrs=send_email,
-                    msg="Subject:test message\n\nHello World")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs=send_email,
+                        msg="Subject:test message\n\nHello World")
