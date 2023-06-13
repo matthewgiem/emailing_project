e89b4cffb25aa4f2b5159472585774fca95e74f5
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-06-13 10:46:19 -0700

    fix connection issue and add subject

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import smtplib
 from secret_variables import password, email
 
-my_email = "matthew.giem@gmail.com"
+my_email = "giemmatthew@gmail.com"
 password = password
+send_email = email
 
-connection = smtplib.SMTP("smtp.gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com", 587)
 connection.starttls()
 connection.login(user=my_email, password=password)
-connection.sendmail()
+connection.sendmail(from_addr=my_email,
+                    to_addrs=send_email,
+                    msg="Subject:test message\n\nHello World")
+connection.close()
