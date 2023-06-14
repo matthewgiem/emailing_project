9e1b47153ad900992140116bcf1c88d267ecb2a4
Matthew Giem <matthew.giem.programmer@gmail.com>
2023-06-14 11:32:58 -0700

    send an email each week containing an inspirational quote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import smtplib
+import random
+import datetime as dt
 from secret_variables import password, email
 
 my_email = "giemmatthew@gmail.com"
@@ -11,3 +13,15 @@ with smtplib.SMTP("smtp.gmail.com", 587) as connection:
     connection.sendmail(from_addr=my_email,
                         to_addrs=send_email,
                         msg="Subject:test message\n\nHello World")
+
+
+data = open("quotes.txt", "r")
+quotes = data.readlines()
+
+
+now = dt.datetime.now()
+if now.weekday() == 2:
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(from_addr=my_email, to_addrs=send_email, msg=quotes[random.randint(0, 101)])
